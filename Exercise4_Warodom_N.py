4946ef4e86bfb86a9e3a748eb6fcd521225f9e1b
warodomnilanon <59312858+warodomnilanon@users.noreply.github.com>
2019-12-31 01:53:52 +0700

    Added score for year 2/1

---
+++ Exercise4_Warodom_N.py
@@ -0,0 +1,8 @@
+FE = 10.5
+GB = 11.2
+CS = 15.6
+CP = 19.8
+print("Foundation English                ", FE)
+print("Gerneral Business                 ", GB)
+print("Introduction to Compputer Systems ", CS)
+print("Coumputer Programming             ", CP)
