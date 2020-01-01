953fd39ebff23ba430e5d6723e9591781fd6aa15
warodomnilanon <59312858+warodomnilanon@users.noreply.github.com>
2020-01-01 13:17:24 +0700

    Lecture50

---
+++ Lecture50_Warodom_N.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def minNumber(x,y):
+    print(x-y)
+def multNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+
+addNumber(10,2)
+minNumber(10,2)
+multNumber(10,2)
+divNumber(10,2)
