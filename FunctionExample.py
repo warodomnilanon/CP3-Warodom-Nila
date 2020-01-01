7ee02be6a555d19d6f6975a97d15aff1b380dad5
warodomnilanon <59312858+warodomnilanon@users.noreply.github.com>
2020-01-01 13:15:05 +0700

    Added FunctionExple for Lecture 50

---
+++ FunctionExample.py
@@ -0,0 +1,26 @@
+'''
+# def + name of function + () + :
+def helloWorld():
+    print("Hello World")
+    helloBig()
+# if want to use fun, just fill name of fun
+def helloBig():
+    print("Hello Big")
+helloWorld()
+helloBig()
+'''
+
+# parameter = () and get data
+def addNumber(x,y):
+    print(x+y)
+def minNumber(x,y):
+    print(x-y)
+def multNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+
+addNumber(10,2)
+minNumber(10,2)
+multNumber(10,2)
+divNumber(10,2)
